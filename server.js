d75282654fdd0291d40d037f6dc551400685e8bb
Sean Ahn <edotakeout@gmail.com>
2024-01-25 22:10:48 -0500

    initial dl npm

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const fs = require('fs');
+const path = require('path');
+const { v4 } = require('uuid');
+
+
+// const DB_PATH = path.join(__dirname, './data.json');
+
+// async function getUserData() {
+//     const users = await fs.promises.readFile(DB_PATH, 'utf8');
+    
+//     return JSON.parse(users);
+
+// }
+
+// async function saveUserData(usersArr) {
+//     await fs.promises.writeFile(DB_PATH, JSON.stringify(usersArr, null, 2));
+
+//     console.log('User Data Updated');
+// }
+
+// module.exports = {
+//     getUserData,
+//     saveUserData
+// }
