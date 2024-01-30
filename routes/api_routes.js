510e576890917e86c2530906ed1491c0f921f05f
Sean Ahn <edotakeout@gmail.com>
2024-01-30 10:00:27 -0500

    updated routes

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -19,14 +19,14 @@ router.get('/notes', (requestObj, responseObj) => {
 
 
 //Create GET route for notes
-router.get('/notes', (requestObj, responseObj) => {
+router.get('/api/notes', (requestObj, responseObj) => {
     const notes = fs.readFileSync('db.json')
     responseObj.send(notes);
 })
 
 
 //Create POST routes for notes
-router.post('/notes', express.json(), (requestObj, responseObj) => {
+router.post('/api/notes', express.json(), (requestObj, responseObj) => {
     const notes = JSON.parse(fs.readFileSync('db.json'))
     const note = requestObj.body
     note.id = uuidv4()
@@ -41,7 +41,7 @@ router.post('/notes', express.json(), (requestObj, responseObj) => {
 
 
 //Create Delete routes for notes
-router.delete('/notes/:id', (requestObj, responseObj) => {
+router.delete('/api/notes/:id', (requestObj, responseObj) => {
     const notes = JSON.parse(fs.readFileSync('db.json'))
     const filterNotes = notes.filter(note => note.id !== requestObj.params.id);
     fs.writeFile ('db.json', JSON.stringify (filterNotes), function (err) {
@@ -50,6 +50,19 @@ router.delete('/notes/:id', (requestObj, responseObj) => {
         }
         responseObj.send(notes)
     });
+
+
+
+//i think i need this???
+// // route for the notes.html file
+// app.get('/notes', (requesstObj, responseObj) =>
+//     responseObj.sendFile(path.join(__dirname, './public/notes.html'))
+// );
+
+// route for the indext.html file
+// app.get('*', (requestObj, responseObj) =>
+//     responseObj.sendFile(path.join(process.cwd(), '/public/index.html'))
+// );
 
 } )
 
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Load in packages
 const express = require('express')
-const api_routes = require('./routes/api_routes');
+const api_routes = require('./routes/api_routes.js');
 
 // This is localhost
 const PORT = 3334
@@ -8,14 +8,15 @@ const app = express();
 
 
 // Opening up the middleware channel to allow json to be sent through from the client
-app.use(express.json());
+// app.use(express.json());
 
 // Share or create a GET route for every file in the public folder (css, html, index.js)
-app.use(express.static('.public'));
+app.use(express.static('public'));
 
 // Load routes
-app.use('/api', api_routes);
+app.use('/', api_routes);
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)
 });
+
